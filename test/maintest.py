3357596eff5c42d2df54bdac2f010c0e31129396
jiawei.li <jiaweiliwork@outlook.com>
2024-05-24 11:41:51 -0600

    modified ignore

--- test/maintest.py
+++ test/maintest.py
@@ -16,3 +16,16 @@ tTEM_center = tt.core.process_ttem.ProcessTTEM(tTEM_file_list[1], DOI_file_list)
 tTEM_north_data = tTEM_north.data()
 tTEM_center_data = tTEM_center.data()
 tTEM_lsl_data = tTEM_lsl.data()
+
+def amountbuy(targetprice, singleprice):
+    amount = targetprice/singleprice
+    return amount
+
+def price_discount(online_price, steam_price, total_price):
+    steam_income = steam_price*0.85
+    amount = amountbuy(total_price, steam_income)
+    steam_total = steam_income*amount
+    online_total = amount*online_price
+    chinese_price = steam_total*7.24
+    discount = online_total/chinese_price
+    print('Buy{}, discount{}'.format(amount,discount))
